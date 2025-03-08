4e63bcdbef8a8e33ef796c4b1e24bdc433911b00
ryan-pienaar <93183950+ryan-pienaar@users.noreply.github.com>
2025-03-08 14:00:55 +0000

    Add menuStrip

--- recipe_organizer/MainWindow.Designer.cs
+++ recipe_organizer/MainWindow.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            menuStrip = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            importToolStripMenuItem = new ToolStripMenuItem();
+            exportToolStripMenuItem = new ToolStripMenuItem();
+            singleRecipeToolStripMenuItem = new ToolStripMenuItem();
+            allRecipesToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(424, 137);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip
+            // 
+            menuStrip.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+            menuStrip.Location = new Point(0, 0);
+            menuStrip.Name = "menuStrip";
+            menuStrip.Size = new Size(927, 24);
+            menuStrip.TabIndex = 1;
+            menuStrip.Text = "menuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { importToolStripMenuItem, exportToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // importToolStripMenuItem
+            // 
+            importToolStripMenuItem.Name = "importToolStripMenuItem";
+            importToolStripMenuItem.Size = new Size(180, 22);
+            importToolStripMenuItem.Text = "Import";
+            // 
+            // exportToolStripMenuItem
+            // 
+            exportToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { singleRecipeToolStripMenuItem, allRecipesToolStripMenuItem });
+            exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            exportToolStripMenuItem.Size = new Size(180, 22);
+            exportToolStripMenuItem.Text = "Export";
+            // 
+            // singleRecipeToolStripMenuItem
+            // 
+            singleRecipeToolStripMenuItem.Name = "singleRecipeToolStripMenuItem";
+            singleRecipeToolStripMenuItem.Size = new Size(180, 22);
+            singleRecipeToolStripMenuItem.Text = "Single Recipe";
+            // 
+            // allRecipesToolStripMenuItem
+            // 
+            allRecipesToolStripMenuItem.Name = "allRecipesToolStripMenuItem";
+            allRecipesToolStripMenuItem.Size = new Size(180, 22);
+            allRecipesToolStripMenuItem.Text = "All Recipes";
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(927, 543);
+            Controls.Add(button1);
+            Controls.Add(menuStrip);
+            MainMenuStrip = menuStrip;
+            Name = "MainWindow";
+            Text = "Recipe Organizer";
+            menuStrip.ResumeLayout(false);
+            menuStrip.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem importToolStripMenuItem;
+        private ToolStripMenuItem exportToolStripMenuItem;
+        private ToolStripMenuItem singleRecipeToolStripMenuItem;
+        private ToolStripMenuItem allRecipesToolStripMenuItem;
     }
 }
